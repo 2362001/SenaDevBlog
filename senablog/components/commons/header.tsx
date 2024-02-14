aea1b216f132f3a7331ef05a2876192ebaff6297
2362001 <o0othanh2k1@gmail.com>
2024-02-14 16:09:03 +0700

    update

--- senablog/components/commons/header.tsx
+++ senablog/components/commons/header.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import senalogo from "@/public/sena-technologies-seeklogo.svg";
+import { useAuth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import { useAuth } from "@clerk/nextjs";
-
+import bellicon from "@/public/bell.svg";
 const HeaderCommon = () => {
   const { isSignedIn } = useAuth();
 
@@ -27,7 +26,18 @@ const HeaderCommon = () => {
       </div>
       <div className="flex  items-center justify-between">
         {isSignedIn ? (
-          <Button className="mr-2">Log out</Button>
+          <>
+            <Button className="mr-2">Create Post</Button>
+            <Image
+              width={25}
+              height={25}
+              className="mr-2 cursor-pointer"
+              alt="#"
+              src={bellicon}
+            />
+            <div className="w-8 h-8 rounded-full bg-blue-500 cursor-pointer"></div>
+            {/* <Button className="mr-2">Log out</Button> */}
+          </>
         ) : (
           <>
             <Button className="mr-2">Log in</Button>
